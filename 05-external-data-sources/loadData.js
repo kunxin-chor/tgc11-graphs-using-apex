00847fbbe8c2c4cae3b0e8e8eeb27ce7df63a64e
kunxin-chor <chorkunxin@yahoo.com>
2021-02-01 03:23:57 +0000

    Display series data via axios

---
+++ 05-external-data-sources/loadData.js
@@ -0,0 +1,5 @@
+async function loadData() {
+    let response = await axios.get('https://raw.githubusercontent.com/apexcharts/apexcharts.js/master/db.json');
+    let series = response.data;
+    return series
+}
